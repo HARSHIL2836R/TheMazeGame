144de1ce48f471692d2cc7fe4ff9f46ee6ddae9f
HARSHIL2836R <harshilsolanki1912@gmail.com>
2024-04-16 12:00:04 +0530

    Primitive menu made

--- modules/menu.py
+++ modules/menu.py
@@ -2,6 +2,7 @@
 import pygame
 import sys
 from modules.button import Button
+from modules.settings import Settings
 
 def show(screen: pygame.Surface):
     """
@@ -11,21 +12,22 @@ def show(screen: pygame.Surface):
     Returns:
         None
     """
-    # light shade of the button  
-    color_light = (170,170,170)
-    # dark shade of the button  
-    color_dark = (100,100,100)
-    
-    screen.fill('red')
-    
-    swidth, sheight = screen.get_rect().width,screen.get_rect().height
+
+    mg_settings = Settings.Menu()
+    screen.fill(mg_settings.screen_color)
     
     #CREATE AND CONFIGURE BUTTONS
-    lvl1_button = Button(screen, "1", color_light, color_dark, 'black', swidth/5, 60, (screen.get_rect().centerx*2/3, screen.get_rect().centery*4/6))
-    lvl2_button = Button(screen, "2", color_light, color_dark,'black', swidth/5, 60, (screen.get_rect().centerx, screen.get_rect().centery*4/6))
-    lvl3_button = Button(screen, "3", color_light, color_dark,'black', swidth/5, 60, (screen.get_rect().centerx*4/3, screen.get_rect().centery*4/6))
+    lvl1_button = Button(screen, "1", mg_settings.active_bt_color, mg_settings.inactive_bt_color, mg_settings.bt_text_color, mg_settings.lvl_button_width, mg_settings.lvl_button_height, (screen.get_rect().centerx*2/3, screen.get_rect().height*4/6))
+    lvl2_button = Button(screen, "2", mg_settings.active_bt_color, mg_settings.inactive_bt_color,mg_settings.bt_text_color, mg_settings.lvl_button_width, mg_settings.lvl_button_height, (screen.get_rect().centerx, screen.get_rect().height*4/6))
+    lvl3_button = Button(screen, "3", mg_settings.active_bt_color, mg_settings.inactive_bt_color,mg_settings.bt_text_color, mg_settings.lvl_button_width, mg_settings.lvl_button_height, (screen.get_rect().centerx*4/3, screen.get_rect().height*4/6))
     #
     
+    font = pygame.font.SysFont("Monospace", 70)
+    title = font.render("THE MAZE GAME", True, mg_settings.screen_text_color, mg_settings.screen_color)
+    font = pygame.font.SysFont("Monospace", 32)
+    selector = font.render("###|Select Level|###", True, mg_settings.screen_text_color, mg_settings.screen_color)
+    to_quit = font.render("Press ESC to QUIT", True, mg_settings.screen_text_color, mg_settings.screen_color)
+
     while True:
         mouse = pygame.mouse.get_pos()
 
@@ -36,6 +38,10 @@ def show(screen: pygame.Surface):
         for button in {lvl1_button,lvl2_button,lvl3_button}:
             button.draw_button()
             button.update_button(mouse[0], mouse[1])
-            
+
+        screen.blit(title, (screen.get_rect().centerx-title.get_rect().width/2,screen.get_rect().height*2/6))
+        screen.blit(selector, (screen.get_rect().centerx-selector.get_rect().width/2,screen.get_rect().height*3/6))
+        screen.blit(to_quit, (screen.get_rect().centerx-to_quit.get_rect().width/2,screen.get_rect().height*5/6))
+
         #Keep Updating the Screen
         pygame.display.update()
--- modules/settings.py
+++ modules/settings.py
@@ -8,3 +8,15 @@ class Settings():
         self.screen_width = 800
         self.screen_height = 800
         self.bg_color = (144,43,245)
+
+    class Menu():
+        def __init__(self) -> None:
+            """Menu Settings"""
+            Settings.__init__(self)
+            self.screen_color = (145,63,146)
+            self.screen_text_color = (255,255,255)
+            self.lvl_button_width = self.screen_width/5
+            self.lvl_button_height = 60
+            self.active_bt_color = 	(0,0,255)
+            self.inactive_bt_color = (35,58,119)
+            self.bt_text_color = (255,255,255)
